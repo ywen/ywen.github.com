126b706d8bb87c7e98fd9ddea2ea6169c5cbeaee
Yi Wen <hayafirst@gmail.com>
2012-04-02 23:12:24 -0500

    Site updated at 2012-04-03 04:12:24 UTC

--- downloads/code/builder.rb
+++ downloads/code/builder.rb
@@ -1,9 +1,11 @@
 module Builders
   class User < Builder
     def build
-      user = BusinessModel::User.build params
-      user.email_addresses = [EmailAddress.new(params).build]
-      user.phones = [Phone.new(params).build]
+      BusinessModel::User.build(params).tap do |business_object|
+        business_object.email_addresses = [EmailAddress.new(params).build]
+        business_object.phones = [Phone.new(params).build]
+        business_object.address = Address.new(params).build
+      end
     end
   end
 end
